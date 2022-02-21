b22962c53dd0f8d8038726c8974509eea4f6f40b
Julen Badiola <33113693+julenbadiola@users.noreply.github.com>
2022-02-21 11:20:02 +0100

    Integration dict

--- interlinkers/software/schema.py
+++ interlinkers/software/schema.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 from interlinkers.base import InterlinkerSchema
 from pydantic import FilePath, HttpUrl, BaseModel
-from typing import Optional, Union
+from typing import Optional, Union, List
 
 parent = Path(__file__).parents[0]
 
@@ -17,39 +17,35 @@ class AuthMethods(Enum):
     header = "header"
     cookie = "cookie"
 
-class Endpoint(BaseModel):
-    service_name: str
-    domain: str
-    path: str
-    is_subdomain: bool
-    api_path: str
 
 class Capabilities(BaseModel):
     instantiate: bool
     view: bool
     edit: bool
     delete: bool
+    open_in_modal: bool
+
+class Integration(BaseModel):
+    service_name: str
+    domain: str
+    path: str
+    is_subdomain: bool
+    api_path: str
+    capabilities: Capabilities
+    auth_method: AuthMethods
 
 class Schema(InterlinkerSchema):
-    supported_by: Supporters
-    auth_method: AuthMethods
+    logotype: Optional[FilePath]
+
+    supported_by: List[Supporters]
+    
     deployment_manual: Optional[Union[HttpUrl, FilePath]]
     user_manual: Optional[Union[HttpUrl, FilePath]]
     developer_manual: Optional[Union[HttpUrl, FilePath]]
 
     supports_internationalization: bool
-    
     is_responsive: bool
-    # GUI is responsive
-    open_in_modal: bool
-    # assets for specific interlinkers may be opened on a modal, not in a new tab
     
-    endpoint: Endpoint
-    capabilities: Capabilities
-    logotype: FilePath
-    # if is_subdomain == true:
-    #   https://{path}.dev.interlink-project.eu
-    #   for example loomio
-    # else:
-    #   https://dev.interlink-project.eu/{path}
-    #   for example googledrive or survey
+    integration: Optional[Integration] 
+    
+    
