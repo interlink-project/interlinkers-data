dc8c52088d415d54b81a4cd27b54439ab0e69581
Julen Badiola <badiolajulen@gmail.com>
2022-05-18 12:28:18 +0200

    Moment

--- react/src/utils/moment.js
+++ react/src/utils/moment.js
@@ -1,8 +1,4 @@
 import moment from "moment";
-import 'moment/src/locale/en-gb'
-import 'moment/src/locale/es'
-import 'moment/src/locale/lv'
-import 'moment/src/locale/it'
 
 export const getLocalizedDate = (language, date) => {
     moment.locale(language === "en" ? "en-gb" : language);
